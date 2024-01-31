3a93895d045bb1aff5049b0eb4c3911befeab72b
J <133668286+alswla@users.noreply.github.com>
2024-01-31 18:05:33 +0900

    Feat: 로고 컴포넌트 개발

---
+++ src/components/shared/WhiteLogo.jsx
@@ -0,0 +1,13 @@
+function WhiteLogo() {
+  return (
+    <div className="flex items-center justify-center">
+      <img
+        src="/assets/krossCutting_logo_white.png"
+        alt="logo"
+        className="h-auto w-300 my-15"
+      />
+    </div>
+  );
+}
+
+export default WhiteLogo;
