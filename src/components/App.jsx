473218e0053652895475f1c033ea6590c11d0fe9
J <133668286+alswla@users.noreply.github.com>
2024-01-31 20:19:50 +0900

    Refactor: App컴포넌트에 배경 및 랜딩페이지 컴포넌트 추가

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
+import VideoBackground from "./shared/VideoBackground";
+import MainPage from "./LandingPage/MainPage";
+
 function App() {
-  return <div></div>;
+  return (
+    <main className="box-border flex flex-col w-screen h-screen">
+      <VideoBackground />
+      <div className="flex items-center justify-center w-full h-full">
+        <MainPage />
+      </div>
+    </main>
+  );
 }
 
 export default App;
