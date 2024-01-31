53eb3e044e4b9a9bb99c3c3de9336f02b5aef0d4
J <133668286+alswla@users.noreply.github.com>
2024-01-31 20:05:52 +0900

    Feat: 랜딩페이지 서비스 소개 컴포넌트 개발

---
+++ src/components/LandingPage/IntroBox.jsx
@@ -0,0 +1,18 @@
+function IntroBox() {
+  return (
+    <>
+      <p className="mb-10 font-bold leading-snug text-center w-500 text-25">
+        Experience <br />
+        your KPOP idol&#39;s various performances <br />
+        in a single video
+      </p>
+      <p className="font-light leading-snug text-center w-500 text-15">
+        Welcome to our platform where we specialize in crafting seamless and
+        innovative video edits that capture individual moments, allowing you to
+        experience the diverse performances of your idols all at once
+      </p>
+    </>
+  );
+}
+
+export default IntroBox;
