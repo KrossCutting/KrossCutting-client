be8e02e02e8c1fc2711f97569a386bcd10c879f5
J <133668286+alswla@users.noreply.github.com>
2024-01-31 18:03:45 +0900

    Style: CSS config 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 
-const px50 = { ...Array.from(Array(11)).map((_, i) => `${i}px`) };
-const px400 = { ...Array.from(Array(201)).map((_, i) => `${i}px`) };
-const px800 = { ...Array.from(Array(701)).map((_, i) => `${i}px`) };
+const px50 = { ...Array.from(Array(51)).map((_, i) => `${i}px`) };
+const px600 = { ...Array.from(Array(601)).map((_, i) => `${i}px`) };
+const px800 = { ...Array.from(Array(801)).map((_, i) => `${i}px`) };
 
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
@@ -12,7 +12,10 @@ export default {
       fontSize: px50,
       width: px800,
       height: px800,
-      spacing: px400,
+      spacing: px600,
+      zIndex: {
+        "-1": "-1",
+      },
     },
   },
   plugins: [],
