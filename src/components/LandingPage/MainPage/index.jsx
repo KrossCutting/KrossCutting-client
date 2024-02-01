11fde1e9288d34b7f0702718de3381f9ec07338f
sht02048 <sht02048@gmail.com>
2024-02-01 20:44:45 +0900

    Feat: 로딩 컴포넌트 생성

--- src/components/LandingPage/MainPage/index.jsx
+++ src/components/LandingPage/MainPage/index.jsx
@@ -1,28 +1,43 @@
+import { useState, useEffect } from "react";
+
 import IntroBox from "../IntroBox";
+import Loading from "../../Loading";
 import UploadForm from "../UploadForm";
-import WhiteLogo from "../../shared/WhiteLogo";
 import SampleVideo from "../SampleVIdeo";
+import WhiteLogo from "../../shared/WhiteLogo";
 import VideoBackground from "../../shared/VideoBackground";
 
 function MainPage() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // To Do 추후 서버 다운로드 구현 로직 완성시 데이터 송신 시 Loading 컴포넌트 언마운트
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  }, [isLoading]);
+
   return (
-    <div className="box-border flex flex-col w-screen h-screen">
-      <VideoBackground />
-      <div className="flex items-center justify-center w-full h-full">
-        <main className="flex flex-col w-full h-full text-white">
-          <div className="flex items-center justify-start">
-            <WhiteLogo />
-          </div>
-          <section className="w-full flex flex-col justify-center items-center py-2.5 my-20">
-            <IntroBox />
-          </section>
-          <section className="flex items-center justify-center w-full space-x-30">
-            <UploadForm />
-            <SampleVideo />
-          </section>
-        </main>
+    <>
+      <div className="box-border flex flex-col w-screen h-screen">
+        <VideoBackground />
+        <div className="flex items-center justify-center w-full h-full">
+          <main className="flex flex-col w-full h-full text-white">
+            <div className="flex items-center justify-start">
+              <WhiteLogo />
+            </div>
+            <section className="w-full flex flex-col justify-center items-center py-2.5 my-20">
+              <IntroBox />
+            </section>
+            <section className="flex items-center justify-center w-full space-x-30">
+              <UploadForm />
+              <SampleVideo />
+            </section>
+          </main>
+        </div>
       </div>
-    </div>
+      {isLoading && <Loading />}
+    </>
   );
 }
 
