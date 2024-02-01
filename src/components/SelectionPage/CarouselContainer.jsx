c8d76b8aec0451a58e5091737c88f6ff9c986ce4
J <133668286+alswla@users.noreply.github.com>
2024-02-01 13:34:32 +0900

    Feat: Carousel 컴포넌트 개발

---
+++ src/components/SelectionPage/CarouselContainer.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import { FaCircleChevronLeft, FaCircleChevronRight } from "react-icons/fa6";
+
+import useUrlStore from "../../store/store";
+
+function CarouselContainer() {
+  const { videoUrls } = useUrlStore();
+  const urlVideoList = Object.values(videoUrls);
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  function handleLeftClick() {
+    setCurrentIndex((prev) => prev - 1);
+  }
+
+  function handleRightClick() {
+    setCurrentIndex((prev) => prev + 1);
+  }
+
+  return (
+    <main className="flex flex-col items-center justify-center w-full h-full">
+      <section className="flex items-center space-x-30">
+        <FaCircleChevronLeft
+          size={40}
+          className={`fill-white ${currentIndex === 0 ? "invisible" : null} cursor-pointer`}
+          onClick={handleLeftClick}
+        />
+        <div className="w-720 h-405 space-y-30">
+          {/* TODO_바다오 컴포넌트로 변경 필요 */}
+          <video
+            key={urlVideoList[currentIndex]}
+            className="rounded-md"
+            autoPlay
+            muted
+            loop
+          >
+            <source src={urlVideoList[currentIndex]} type="video/mp4" />
+          </video>
+          {/* TODO_오디오 컴포넌트 포함 필요 */}
+        </div>
+        <FaCircleChevronRight
+          size={40}
+          className={`fill-white ${currentIndex === 2 ? "invisible" : null} cursor-pointer`}
+          onClick={handleRightClick}
+        />
+      </section>
+    </main>
+  );
+}
+
+export default CarouselContainer;
