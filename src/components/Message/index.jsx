32c2946e20f75cebb6ec000cc3659e1b9d2c0c6c
sht02048 <sht02048@gmail.com>
2024-02-01 19:29:07 +0900

    Refactor: 상수 문구 로직 수정

--- src/components/Message/index.jsx
+++ src/components/Message/index.jsx
@@ -1,22 +1,13 @@
 import PropTypes from "prop-types";
 
-import {
-  ALERT_MESSAGE,
-  MAIN_MESSAGE,
-  CHECK_MESSAGE,
-} from "../../constants/message";
-
 function Message({ messageType }) {
-  const MESSAGES = ["QUALITY", "PLAYTIME", "START_POINT"];
-  const selectedMessage = MESSAGES.filter((value) => value === messageType);
+  // TO DO 상황에 따른 button의 onClick 함수 생성 필요. prerequisite: 필요 컴포넌트 라우터 설정
 
   return (
     <section className="flex flex-col items-center py-20 text-xl text-center gap-15 px-25 rounded-3xl w-480 h-270 bg-pink">
-      <span className="text-2xl font-bold">
-        {ALERT_MESSAGE[selectedMessage]}
-      </span>
-      <span>{MAIN_MESSAGE[selectedMessage]}</span>
-      <span>{CHECK_MESSAGE[selectedMessage]}</span>
+      <p className="text-2xl font-bold">{messageType.HEADER}</p>
+      <p>{messageType.BODY}</p>
+      <p>{messageType.FOOTER}</p>
       <div className="flex justify-around w-full mt-5 text-base">
         <button className="px-10 py-5 rounded-3xl bg-purple">
           Go to Selection
@@ -28,7 +19,7 @@ function Message({ messageType }) {
 }
 
 Message.propTypes = {
-  messageType: PropTypes.string.isRequired,
+  messageType: PropTypes.instanceOf(Object).isRequired,
 };
 
 export default Message;
--- src/constants/message.js
+++ src/constants/message.js
@@ -1,20 +1,19 @@
-const ALERT_MESSAGE = {
-  QUALITY: "No Quality Found Above 720p",
-  PLAYTIME: "Video Is Too Long",
-  START_TIME: "Start Point Not Selected",
+const QUALITY_MESSAGE = {
+  HEADER: "No Quality Found Above 720p",
+  BODY: "Some of the URLs you provided don't have qualities above 720p",
+  FOOTER: "Do you want to proceed with low quality?",
 };
 
-const MAIN_MESSAGE = {
-  QUALITY: "Some of the URLs you provided don't have qualities above 720p",
-  PLAYTIME:
-    "Some of the video you provided are too long to proceed Kross-cutting",
-  START_TIME: "The start point of the main video has not been selected",
+const PLAYTIME_ALERT = {
+  HEADER: "Video Is Too Long",
+  BODY: "Some of the video you provided are too long to proceed Kross-cutting",
+  FOOTER: "Please ensure other video URLs have a playtime below 5 minutes",
 };
 
-const CHECK_MESSAGE = {
-  QUALITY: "Do you want to proceed with low quality?",
-  PLAYTIME: "Please ensure other video URLs have a playtime below 5 minutes",
-  START_TIME: "Please select start point to proceed Kross-cutting",
+const START_POINT_ALERT = {
+  HEADER: "Start Point Not Selected",
+  BODY: "The start point of the main video has not been selected",
+  FOOTER: "Please select start point to proceed Kross-cutting",
 };
 
-export { ALERT_MESSAGE, MAIN_MESSAGE, CHECK_MESSAGE };
+export { QUALITY_MESSAGE, PLAYTIME_ALERT, START_POINT_ALERT };
