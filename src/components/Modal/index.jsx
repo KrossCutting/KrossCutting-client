73d2c49e2674053afc04450aeab346b74bda58a1
sht02048 <sht02048@gmail.com>
2024-02-01 11:10:42 +0900

    Feat: modal 컴포넌트 생성

---
+++ src/components/Modal/index.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+
+function Modal({ children }) {
+  return (
+    <div>
+      <Link to="..">
+        <div className="top-0 left-0 w-screen h-screen"></div>
+      </Link>
+      <div className="max-w-full overflow-y-auto transform -translate-x-1/2 -translate-y-1/2 rounded-lg ixed top-1/2 left-1/2 w-80 md:w-full max-h-90vh">
+        {children}
+      </div>
+    </div>
+  );
+}
+
+export default Modal;
