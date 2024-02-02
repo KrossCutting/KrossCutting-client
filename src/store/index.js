ab1917c0e5643bf8083e67ad655f828735767a5c
zzinLee <sujin951017@gmail.com>
2024-02-02 13:18:14 +0900

    Refactor: zustand 전역 상태 구조 변경

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,28 @@
 import { create } from "zustand";
+import { devtools } from "zustand/middleware";
 
-const useUrlStore = create((set) => ({
+const urlStore = (set) => ({
   videoUrls: { mainVideoUrl: "", firstSubVideoUrl: "", lastSubVideoUrl: "" },
+  audioUrls: { mainAudioUrl: "", firstSubAudioUrl: "", lastSubAudioUrl: "" },
+  startPoints: {
+    mainStartPoint: undefined,
+    firstSubStartPoint: undefined,
+    lastSubStartPoint: undefined,
+  },
   setVideoUrls: (mainVideoUrl, firstSubVideoUrl, lastSubVideoUrl) =>
     set({ videoUrls: { mainVideoUrl, firstSubVideoUrl, lastSubVideoUrl } }),
-}));
+  setAudioUrls: (mainAudioUrl, firstSubAudioUrl, lastSubAudioUrl) =>
+    set({ audioUrls: { mainAudioUrl, firstSubAudioUrl, lastSubAudioUrl } }),
+  setStartPoints: ({ mainStartPoint, firstSubStartPoint, lastSubStartPoint }) =>
+    set({
+      startPoints: {
+        mainStartPoint,
+        firstSubStartPoint,
+        lastSubStartPoint,
+      },
+    }),
+});
+
+const useUrlStore = create(devtools(urlStore));
 
 export default useUrlStore;
