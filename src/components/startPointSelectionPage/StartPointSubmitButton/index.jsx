f3041bb553bc0def54e36e8cbcab0060c5c54d45
zzinLee <sujin951017@gmail.com>
2024-02-02 13:27:09 +0900

    Feat: 시작점 선택 페이지 버튼 컴포넌트 생성

---
+++ src/components/startPointSelectionPage/StartPointSubmitButton/index.jsx
@@ -0,0 +1,14 @@
+function StartPointSubmitButton() {
+  // TODO: 서버와 연결 후, axios post endpoint로 사용자가 선택한 시작점을 제출합니다.
+
+  return (
+    <button
+      type="button"
+      className="text-white bg-purple rounded-lg text-sm px-5 py-2.5 mb-2"
+    >
+      next step
+    </button>
+  );
+}
+
+export default StartPointSubmitButton;
