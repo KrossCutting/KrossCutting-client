495b741c737c7286141689364091e649dca870bb
J <133668286+alswla@users.noreply.github.com>
2024-02-11 02:54:18 +0900

    Refactor: Message 컴포넌트와 axios 재요청 로직 분리, 컴포넌트별 구현

--- src/components/Message/index.jsx
+++ src/components/Message/index.jsx
@@ -1,81 +1,63 @@
-import { useState } from "react";
-
-import axios from "axios";
 import PropTypes from "prop-types";
-import { useNavigate } from "react-router-dom";
 
-import API from "../../../config";
 import Modal from "../shared/Modal";
-import Loading from "../shared/Loading";
-import { VERIFICATION_MESSAGE } from "../../constants/message";
+import { useStartPointStore, useAwsVideoStore } from "../../store";
 import {
-  useAwsVideoStore,
-  useAwsAudioStore,
-  useStartPointStore,
-  useYouTubeUrlStore,
-} from "../../store";
+  AUDIO_ALERT,
+  PLAYTIME_ALERT,
+  PROCEED_MESSAGE,
+} from "../../constants/message";
 
-function Message({ messageType }) {
-  const navigate = useNavigate();
-  const [isLoading, setIsLoading] = useState(false);
-  const { startPoints } = useStartPointStore();
-  const { youtubeUrls } = useYouTubeUrlStore();
-  const { setVideoUrls } = useAwsVideoStore();
-  const { setAudioUrls } = useAwsAudioStore();
+function Message({ messageType, handleProceedClick, handleSelectionClick }) {
+  const { startPoints } = useStartPointStore.getState();
+  const { videoUrls } = useAwsVideoStore.getState();
+  const { HEADER, BODY, FOOTER } = messageType;
 
-  async function handleProcessClick() {
-    if (messageType === VERIFICATION_MESSAGE) {
-      const response = await axios.post(API.COMPILATIONS, { startPoints });
-      // To Do resopnse 받으면 영상 다운 받을 수 있게 처리
-      return;
-    }
+  const isProceedDisabled =
+    messageType === AUDIO_ALERT || messageType === PLAYTIME_ALERT;
 
-    setIsLoading(true);
+  const startPointMessageBody = `Main video: +${startPoints.mainStartPoint} sec,
+    \nSub video 1: +${startPoints.subOneStartPoint} sec,
+    ${
+      videoUrls.subTwoVideoUrl
+        ? `\nSub video 2: +${startPoints.subTwoStartPoint} sec`
+        : ""
+    }`;
 
-    const response = await axios.post(API.CONTENTS, {
-      videoUrls: youtubeUrls,
-      isPermitted: true,
-    });
-
-    const { result } = response.data;
-
-    if (result === "success") {
-      const [mainVideoUrl, subOneVideoUrl, subTwoVideoUrl] =
-        response.data.videoUrlList;
-      const [mainAudioUrl, subOneAudioUrl, subTwoAudioUrl] =
-        response.data.audioUrlList;
-
-      setVideoUrls(mainVideoUrl, subOneVideoUrl, subTwoVideoUrl);
-      setAudioUrls(mainAudioUrl, subOneAudioUrl, subTwoAudioUrl);
-      setIsLoading(false);
-      navigate("/selection");
-    }
+  if (messageType === PROCEED_MESSAGE) {
+    PROCEED_MESSAGE.BODY = startPointMessageBody;
   }
 
   return (
     <>
+      {console.log(isProceedDisabled)}
       <Modal>
-        <section className="z-50 flex flex-col items-center py-20 text-xl text-center px-25 gap-15 rounded-3xl bg-pink">
-          <p className="text-2xl font-bold">{messageType.HEADER}</p>
-          <p>{messageType.BODY}</p>
-          <p>{messageType.FOOTER}</p>
-          <div className="flex justify-around w-full mt-5 text-base">
+        <main className="modal-wrapper">
+          <section className="space-y-20 text-white">
+            <p className="font-bold text-25">{HEADER}</p>
+            <div className="space-y-5">
+              <p className="font-thin text-20">{BODY}</p>
+              <p className="font-thin text-20">{FOOTER}</p>
+            </div>
+          </section>
+          <section className="flex mt-5 font-bold space-x-30 jw-full">
             <button
-              className="px-10 py-5 rounded-3xl bg-purple"
-              onClick={() => navigate(-1)}
+              className="button-selection"
+              onClick={() => handleSelectionClick(null)}
             >
               Go to Selection
             </button>
-            <button
-              className="px-10 py-5 rounded-3xl bg-mint"
-              onClick={handleProcessClick}
-            >
-              Yes, proceed
-            </button>
-          </div>
-        </section>
+            {!isProceedDisabled && (
+              <button
+                className="button-submission"
+                onClick={handleProceedClick}
+              >
+                Yes, proceed
+              </button>
+            )}
+          </section>
+        </main>
       </Modal>
-      {isLoading && <Loading />}
     </>
   );
 }
