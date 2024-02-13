d2659918aca65cd41d5fefd904350b14354c9ee1
sht02048 <sht02048@gmail.com>
2024-02-13 19:18:50 +0900

    Refactor: 앤드 포인트 이름 변경

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 const SERVER_URL = import.meta.env.VITE_SERVER_HOST;
 
 const API = {
-  CONTENTS: `${SERVER_URL}/videos/contents`,
+  URLS: `${SERVER_URL}/videos/contents/urls`,
+  FILES: `${SERVER_URL}/videos/contents/files`,
   COMPILATIONS: `${SERVER_URL}/videos/compilations`,
 };
 
