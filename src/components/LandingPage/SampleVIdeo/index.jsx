b48fe3a66d8a28c79c818254febb859f49d88c30
sht02048 <sht02048@gmail.com>
2024-05-31 14:52:39 +0900

    Design: 샘플 영상 소개 문구 추가

--- src/components/LandingPage/SampleVIdeo/index.jsx
+++ src/components/LandingPage/SampleVIdeo/index.jsx
@@ -1,6 +1,6 @@
 function SampleVideo() {
   return (
-    <section className="flex items-center justify-center">
+    <section className="flex flex-col items-center justify-center">
       <div className="min-w-400 max-w-400 max-h-225 min-h-225 w-[48vw] h-[27vw] ml-7">
         <video
           className="relative w-full h-full rounded -z-1"
@@ -11,6 +11,9 @@ function SampleVideo() {
         >
           <source src="/videos/IVE_sample.mp4" />
         </video>
+      </div>
+      <div className="mt-10 py-4 px-8 bg-[rgba(255,255,255,0.1)] rounded-lg text-10">
+        This video is edited using KrossCutting
       </div>
     </section>
   );
